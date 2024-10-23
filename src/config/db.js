e64c2bcb09e5e3d286ea031bc3b5c803f8e2f6c5
Graniska <nataly.granisk@gmail.com>
2024-10-23 13:16:39 -0300

    servidor atualizado

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,30 @@
 
-        // este modulo será utilizado apenas para conectar o banco de dados
-        // para isto utilizamos o mongoose, que é uma ferramenta que facilita a maneira como os documentos são armazenados
-        const mongoose = require ("mongoose") 
+
+      /*  const mongoose = require ("mongoose") 
 
             // esta função assíncrona faz a conexão com o banco
             const connectDB = async () => {
                
                  mongoose
-                    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_ADRESS}/${process.DB_NAME}`)   // o programa vai tentar conectar ao mongo    
-                    .then(() => console.log('Connected to mongoDB'))  // se a ação for bem sucedida, a seguinte mensagem será impressa 
-                    .catch((error) => console.log(error));      // caso o contrário, o erro será mostrado na tela
+                    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_ADRESS}/${process.DB_NAME}`)  
+                    .then(() => console.log('Connected to mongoDB')) 
                 
         }
 
-    module.exports = connectDB;
+    module.exports = connectDB;*/
+
+    const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_ADRESS}/${process.env.DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+module.exports = connectDB;
