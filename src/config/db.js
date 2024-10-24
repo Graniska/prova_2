0cb72229a7f27abfd04458e9e4e430c3693df2a3
Graniska <nataly.granisk@gmail.com>
2024-10-24 14:40:08 -0300

    atualizado

--- src/config/db.js
+++ src/config/db.js
@@ -13,7 +13,7 @@
 
     module.exports = connectDB;*/
 
-    const mongoose = require("mongoose");
+    /*const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
@@ -27,4 +27,21 @@ const connectDB = async () => {
     }
 };
 
+module.exports = connectDB;*/
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_ADDRESS}/${process.env.DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
 module.exports = connectDB;
+
